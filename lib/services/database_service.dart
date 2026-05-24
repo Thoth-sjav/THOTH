ae44bf0d8830354d1c8df0705ed31e7ad1bccca6
Thoth-sjav <thoth.sjav@gmail.com>
2026-05-24 23:41:43 +0100

    Update database_service.dart

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -4,44 +4,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 class DatabaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  DocumentReference<Map<String, dynamic>> _ref(String uid) {
+  DocumentReference<Map<String, dynamic>> _ref() {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
     return _db.collection('users').doc(uid);
   }
 
   Future<Map<String, dynamic>?> getUser() async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-
-    final doc = await _ref(uid).get();
+    final doc = await _ref().get();
     return doc.data();
   }
 
-  Future<void> updateProfile(Map<String, dynamic> data) async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-
-    await _ref(uid).set(data, SetOptions(merge: true));
+  Future<void> updateUser(Map<String, dynamic> data) async {
+    await _ref().set(data, SetOptions(merge: true));
   }
 
   Future<void> updateSettings(Map<String, dynamic> settings) async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-
-    await _ref(uid).set({
+    await _ref().set({
       'settings': settings,
     }, SetOptions(merge: true));
-  }
-
-  Future<void> addXP(int xp) async {
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-
-    await _db.runTransaction((tx) async {
-      final ref = _ref(uid);
-      final snap = await tx.get(ref);
-
-      final data = snap.data() ?? {};
-      final stats = data['stats'] ?? {'xp': 0};
-
-      tx.update(ref, {
-        'stats.xp': (stats['xp'] ?? 0) + xp,
-      });
-    });
   }
 }
