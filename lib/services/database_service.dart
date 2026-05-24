1e4a9f3d44082a87eb0e61cf2d516bc9753fc91c
Thoth-sjav <thoth.sjav@gmail.com>
2026-05-24 23:30:03 +0100

    Update database_service.dart

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,54 +1,47 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DatabaseService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
 
   DocumentReference<Map<String, dynamic>> _ref(String uid) {
-    return _firestore.collection('utilizadores').doc(uid);
+    return _db.collection('users').doc(uid);
   }
 
-  /// LER UTILIZADOR
-  Future<Map<String, dynamic>?> getUser(String uid) async {
+  Future<Map<String, dynamic>?> getUser() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
     final doc = await _ref(uid).get();
     return doc.data();
   }
 
-  /// ATUALIZAR PERFIL
-  Future<void> updateProfile(
-      String uid, Map<String, dynamic> data) async {
+  Future<void> updateProfile(Map<String, dynamic> data) async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
     await _ref(uid).set(data, SetOptions(merge: true));
   }
 
-  /// ATUALIZAR SETTINGS
-  Future<void> updateSettings(
-      String uid, Map<String, dynamic> settings) async {
+  Future<void> updateSettings(Map<String, dynamic> settings) async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+
     await _ref(uid).set({
       'settings': settings,
     }, SetOptions(merge: true));
   }
 
-  /// ADICIONAR XP
-  Future<void> addXP(String uid, int xp) async {
-    await _firestore.runTransaction((tx) async {
-      final doc = await tx.get(_ref(uid));
+  Future<void> addXP(int xp) async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
 
-      final data = doc.data() ?? {};
+    await _db.runTransaction((tx) async {
+      final ref = _ref(uid);
+      final snap = await tx.get(ref);
+
+      final data = snap.data() ?? {};
       final stats = data['stats'] ?? {'xp': 0};
 
-      final currentXP = stats['xp'] ?? 0;
-
-      tx.update(_ref(uid), {
-        'stats.xp': currentXP + xp,
+      tx.update(ref, {
+        'stats.xp': (stats['xp'] ?? 0) + xp,
       });
     });
-  }
-
-  /// INCREMENTAR TASKS
-  Future<void> incrementTasks(String uid) async {
-    await _ref(uid).set({
-      'stats': {
-        'tasks': FieldValue.increment(1),
-      }
-    }, SetOptions(merge: true));
   }
 }
